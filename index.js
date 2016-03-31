58c3917b822478e87da903a305f75723a9aa5f1e
unknown <boronin@boronin2PC.vista.local>
2016-03-31 16:18:30 +0500

    CHANGE error response on validation

--- index.js
+++ index.js
@@ -13,26 +13,36 @@ Scheme.prototype.validate = function (data) {
         var field = this.scheme[key];
 
         if (field.structure) {
-            if (field.structure && field.hasOwnProperty('type') && field.type.toLowerCase() !== "object") return { success : false, error : { key : key, text : 'Wrong type for field with structure!' } };
-            if (field.structure && typeof field.structure !== "object") return { success : false, error : { key : key, text : 'Wrong data for structure!' } };
+            if (field.structure && field.hasOwnProperty('type') && field.type.toLowerCase() !== "object") return error(key, 'Wrong type for field with structure!');
+            if (field.structure && typeof field.structure !== "object") return error(key, 'Wrong data for structure!');
 
             var scheme = new Scheme(field.structure);
             var result = scheme.validate(data[key]);
-            if (!result.success) return { success : false, error : { key : key + '.' + result.error.key, text : result.error.text } };
+            if (!result.success) return error(key + '.' + result.error.key, result.error.text);
         }
 
-        if (!require('./lib/required')(field, data, key)) return { success : false, error : { key : key, text : 'Field is required!' } };
-        if (!require('./lib/type')(field, data, key)) return { success : false, error : { key : key, text : 'Wrong type of field!' } };
+        if (!require('./lib/required')(field, data, key)) return error(key, 'Field is required!');
+        if (!require('./lib/type')(field, data, key)) return error(key, 'Wrong type of field!');
 
-        if (field.enum && field.enum.constructor && field.enum.constructor !== Array) return { success : false, error : { key : key, text : 'Wrong data for enum!' } };
-        if (!require('./lib/enum')(field, data, key)) return { success : false, error : { key : key, text : 'Value is not part of enum!' } };
+        if (field.enum && field.enum.constructor && field.enum.constructor !== Array) return error(key, 'Wrong data for enum!');
+        if (!require('./lib/enum')(field, data, key)) return error(key, 'Value is not part of enum!');
 
-        if (!require('./lib/value')(field, data, key)) return { success : false, error : { key : key, text : 'Value is not equal!' } };
+        if (!require('./lib/value')(field, data, key)) return error(key, 'Value is not equal!');
 
-        if (field.validator && typeof field.validator !== "function") return { success : false, error : { key : key, text : 'Wrong data for validator!' } };
-        if (!require('./lib/validator')(field, data, key)) return { success : false, error : { key : key, text : 'Validator detected wrong data!' } };
+        if (field.validator && typeof field.validator !== "function") return error(key, 'Wrong data for validator!');
+        if (!require('./lib/validator')(field, data, key)) return error(key, 'Validator detected wrong data!');
 
     }
 
     return { success : true };
 };
+
+function error(key, text) {
+    return {
+        success : false,
+        error : {
+            key  : key,
+            text : text
+        }
+    }
+}
